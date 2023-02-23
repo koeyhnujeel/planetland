7bad9e7e97c06999b226771de1b0c8188d38596c
zunza <fksfks015@naver.com>
2023-02-24 01:11:22 +0900

    Create PlanetServiceTest.java

---
+++ src/test/java/com/myproject/planetland/service/PlanetServiceTest.java
@@ -0,0 +1,53 @@
+package com.myproject.planetland.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import com.myproject.planetland.domain.Planet;
+import com.myproject.planetland.domain.User;
+import com.myproject.planetland.repository.PlanetRepository;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class PlanetServiceTest {
+
+	@Autowired
+	PlanetRepository planetRepository;
+
+	@Autowired
+	PlanetService planetService;
+
+	@Test
+	@DisplayName("유저 아이디로 소유 행성")
+	void getPlanetByUserId() {
+		Planet planet1 = new Planet();
+		Planet planet2 = new Planet();
+
+		planet1.setPlanetName("sun");
+		planet2.setPlanetName("earth");
+		Planet saved1 = planetRepository.save(planet1);
+		Planet saved2 = planetRepository.save(planet2);
+
+		User user = new User();
+		user.setEmail("test@email.com");
+		user.setPassword("1234");
+		user.setNickname("zunza");
+		List<Planet> planets = user.getPlanets();
+		planets.add(saved1);
+		planets.add(saved2);
+		user.setPlanets(planets);
+
+		List<Planet> planetList = planetService.getPlanetByUserId(user.getUserId());
+		for (Planet planet : planetList) {
+			System.out.println(planet);
+		}
+	}
+}
