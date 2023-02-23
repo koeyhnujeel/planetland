2838da4e9e84e6da460d9f65fa0c07df2d958c56
zunza <fksfks015@naver.com>
2023-02-23 16:19:59 +0900

    test commit

---
+++ src/main/java/com/myproject/planetland/PlanetlandApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.planetland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/planetland/PlanetlandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.planetland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
