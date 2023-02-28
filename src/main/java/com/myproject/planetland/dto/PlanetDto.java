278f6d41add4e66478e2c674672c7484deada019
zunza <fksfks015@naver.com>
2023-03-01 01:33:34 +0900

    Create PlanetDto.java

---
+++ src/main/java/com/myproject/planetland/dto/PlanetDto.java
@@ -0,0 +1,17 @@
+package com.myproject.planetland.dto;
+
+import com.myproject.planetland.constants.PlanetStatus;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PlanetDto {
+
+	private Long planetId;
+	private String planetName;
+	private int population;
+	private int value;
+	private PlanetStatus planetStatus;
+	private Long userId;
+}
