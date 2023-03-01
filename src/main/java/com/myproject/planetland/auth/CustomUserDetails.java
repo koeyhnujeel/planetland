4bb00eb50114932688924b78adc2fd493394e5ca
zunza <fksfks015@naver.com>
2023-03-02 02:45:39 +0900

    Create CustomUserDetails.java

---
+++ src/main/java/com/myproject/planetland/auth/CustomUserDetails.java
@@ -0,0 +1,61 @@
+package com.myproject.planetland.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.myproject.planetland.domain.User;
+
+import lombok.Data;
+
+@Data
+public class CustomUserDetails implements UserDetails {
+
+	private final User user;
+
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collect = new ArrayList<>();
+        collect.add(new SimpleGrantedAuthority(user.getRole().toString()));
+        return collect;
+	}
+
+	@Override
+	public String getPassword() {
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+}
