d8decf196c08e5b3acce3d2babaca26e8b4a48ad
zunza <fksfks015@naver.com>
2023-03-02 00:09:11 +0900

    Update User.java

--- src/main/java/com/myproject/planetland/domain/User.java
+++ src/main/java/com/myproject/planetland/domain/User.java
@@ -3,8 +3,11 @@ package com.myproject.planetland.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +16,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.myproject.planetland.constants.Role;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -29,16 +34,15 @@ public class User {
 	@Column(name = "user_id", unique = true, nullable = false)
 	private Long userId;
 
-	private String nickname;
-
+	private String userName;
 	private String email;
-
 	private String password;
+	private int asset;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
 	private List<Planet> planets = new ArrayList<>();
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id")
-	private Wallet wallet;
 }
