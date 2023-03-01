610947db7a387e1cb9322b6cdb7e76245be4eb51
zunza <fksfks015@naver.com>
2023-03-02 02:46:03 +0900

    Update UserRepository.java

--- src/main/java/com/myproject/planetland/repository/UserRepository.java
+++ src/main/java/com/myproject/planetland/repository/UserRepository.java
@@ -10,6 +10,7 @@ import com.myproject.planetland.domain.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	Optional<User> findByEmail(String email);
-
 	Optional<User> findByUserNameOrEmail(String userName, String email);
+
+	Optional<User> findByUserName(String userName);
 }
