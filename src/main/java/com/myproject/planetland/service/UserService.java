77bac52eccd27061838498ef322a4ca85f527acd
zunza <fksfks015@naver.com>
2023-03-02 00:08:55 +0900

    Update UserService.java

--- src/main/java/com/myproject/planetland/service/UserService.java
+++ src/main/java/com/myproject/planetland/service/UserService.java
@@ -6,7 +6,11 @@ import javax.persistence.EntityNotFoundException;
 
 import org.springframework.stereotype.Service;
 
+import com.myproject.planetland.constants.Money;
+import com.myproject.planetland.constants.Role;
 import com.myproject.planetland.domain.User;
+import com.myproject.planetland.dto.UserJoinDto;
+import com.myproject.planetland.mapper.UserMapper;
 import com.myproject.planetland.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,6 +20,7 @@ import lombok.RequiredArgsConstructor;
 public class UserService {
 
 	private final UserRepository userRepository;
+	private UserMapper mapper;
 
 	public User getUser(Long userId) {
 		Optional<User> res = userRepository.findById(userId);
@@ -32,6 +37,18 @@ public class UserService {
 			return res.get();
 		} else {
 			throw new EntityNotFoundException(String.format("%s로 조회되는 회원이 없습니다.", email));
+		}
+	}
+
+	public UserJoinDto createUser(UserJoinDto userJoinDto) {
+
+		Optional<User> res = userRepository.findByUserNameOrEmail(userJoinDto.getUserName(), userJoinDto.getEmail());
+		if (res.isPresent()) {
+			throw new IllegalArgumentException("이미 가입된 회원입니다.");
+		} else {
+			userJoinDto.setAsset(Money.INITIAL_FUNDING);
+			userJoinDto.setRole(Role.ROLE_USER);
+			return userJoinDto;
 		}
 	}
 }
