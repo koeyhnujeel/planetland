48f487d9ce0b97814aa1fc7d613ee7f78ec98c23
zunza <fksfks015@naver.com>
2023-03-02 00:09:13 +0900

    Update MainController.java

--- src/main/java/com/myproject/planetland/controller/MainController.java
+++ src/main/java/com/myproject/planetland/controller/MainController.java
@@ -1,17 +1,32 @@
 package com.myproject.planetland.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.myproject.planetland.dto.UserDto;
+import com.myproject.planetland.domain.User;
+import com.myproject.planetland.dto.UserJoinDto;
+import com.myproject.planetland.mapper.UserMapper;
+import com.myproject.planetland.repository.UserRepository;
+import com.myproject.planetland.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/")
+@Slf4j
 public class MainController {
+
+	@Autowired UserMapper mapper;
+	@Autowired UserRepository userRepository;
+	@Autowired UserService userService;
 
 	@GetMapping
 	public String home() {
@@ -23,14 +38,23 @@ public class MainController {
 		return "loginForm";
 	}
 
-	@GetMapping("signup")
-	public String signUpForm(Model model) {
-		model.addAttribute("userDto", new UserDto());
-		return "signupForm";
+	@GetMapping("join")
+	public String joinForm(Model model) {
+		model.addAttribute("userJoinDto", new UserJoinDto());
+		return "joinForm";
 	}
 
-	// @PostMapping("signup")
-	// public String signUpForm2(@ModelAttribute UserDto userDto) {
-	//
-	// }
+	@PostMapping("join")
+	public String joinForm(@ModelAttribute @Valid UserJoinDto userJoinDto, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			log.info("errors = {}", bindingResult);
+			return "joinForm";
+		}
+
+		UserJoinDto res = userService.createUser(userJoinDto);
+		User user = mapper.userJoinDtoToModel(res);
+		userRepository.save(user);
+
+		return "redirect:/";
+	}
 }
