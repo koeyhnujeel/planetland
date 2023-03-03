3389f1ac99c3234a777ba3535b959f0cc0a9ec2c
zunza <fksfks015@naver.com>
2023-03-03 18:18:17 +0900

    Update MainController.java

--- src/main/java/com/myproject/planetland/controller/MainController.java
+++ src/main/java/com/myproject/planetland/controller/MainController.java
@@ -1,65 +1,37 @@
 package com.myproject.planetland.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.myproject.planetland.domain.User;
-import com.myproject.planetland.dto.UserJoinDto;
-import com.myproject.planetland.mapper.UserMapper;
-import com.myproject.planetland.repository.UserRepository;
-import com.myproject.planetland.service.UserService;
+import com.myproject.planetland.domain.Planet;
+import com.myproject.planetland.dto.PlanetListDto;
+import com.myproject.planetland.mapper.PlanetMapper;
+import com.myproject.planetland.service.PlanetService;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/")
-@Slf4j
 public class MainController {
 
-	@Autowired UserMapper mapper;
-	@Autowired UserRepository userRepository;
-	@Autowired UserService userService;
+	@Autowired PlanetService planetService;
+	@Autowired PlanetMapper mapper;
 
 	@GetMapping
-	public String home() {
+	public String home(Model model) {
+		List<Planet> planetList = planetService.getAllPlanet();
+		List<PlanetListDto> planetListDto = mapper.ModelToDtoList(planetList);
+
+		model.addAttribute("planetListDto", planetListDto);
+
 		return "main";
-	}
-
-	@GetMapping("login")
-	public String loginForm() {
-		return "loginForm";
-	}
-
-	@GetMapping("join")
-	public String joinForm(Model model) {
-		model.addAttribute("userJoinDto", new UserJoinDto());
-		return "joinForm";
-	}
-
-	@PostMapping("join")
-	public String joinForm(@ModelAttribute @Valid UserJoinDto userJoinDto, BindingResult bindingResult, Model model) {
-		if (bindingResult.hasErrors()) {
-			log.info("errors = {}", bindingResult);
-			return "joinForm";
-		}
-
-		try {
-			UserJoinDto res = userService.createUser(userJoinDto);
-			User user = mapper.userJoinDtoToModel(res);
-			userRepository.save(user);
-		} catch (IllegalArgumentException e) {
-			model.addAttribute("errorMessage", e.getMessage());
-			log.info("errorMessage = {}", e.getMessage());
-			return "joinForm";
-		}
-		return "redirect:/";
 	}
 }
