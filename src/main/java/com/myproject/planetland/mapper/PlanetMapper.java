34785c9a941cebf2b989dcae25a88f91e6ca5cc7
zunza <fksfks015@naver.com>
2023-03-03 18:18:32 +0900

    Update PlanetMapper.java

--- src/main/java/com/myproject/planetland/mapper/PlanetMapper.java
+++ src/main/java/com/myproject/planetland/mapper/PlanetMapper.java
@@ -1,29 +1,17 @@
 package com.myproject.planetland.mapper;
 
+import java.util.List;
+
+import org.mapstruct.Mapper;
 
 import com.myproject.planetland.domain.Planet;
+import com.myproject.planetland.dto.AddPlanetDto;
 import com.myproject.planetland.dto.PlanetDto;
+import com.myproject.planetland.dto.PlanetListDto;
 
-
-public class PlanetMapper {
-
-	public static PlanetDto convertToDto(Planet planet) {
-		PlanetDto planetDto = new PlanetDto();
-		planetDto.setPlanetId(planet.getPlanetId());
-		planetDto.setPlanetName(planet.getPlanetName());
-		planetDto.setPopulation(planetDto.getPopulation());
-		planetDto.setValue(planet.getValue());
-		planetDto.setPlanetStatus(planet.getPlanetStatus());
-		return planetDto;
-	}
-
-	public static Planet convertToModel(PlanetDto planetDto) {
-		Planet planet = new Planet();
-		planet.setPlanetId(planetDto.getPlanetId());
-		planet.setPlanetName(planetDto.getPlanetName());
-		planet.setPopulation(planetDto.getPopulation());
-		planet.setValue(planetDto.getValue());
-		planet.setPlanetStatus(planetDto.getPlanetStatus());
-		return planet;
-	}
+@Mapper(componentModel = "spring")
+public interface PlanetMapper {
+	Planet addPlanetDtoToModel(AddPlanetDto addPlanetDto);
+	PlanetDto ModelToDto(Planet planet);
+	List<PlanetListDto> ModelToDtoList(List<Planet> planets);
 }
