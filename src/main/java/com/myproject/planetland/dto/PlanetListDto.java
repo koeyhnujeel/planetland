6394c623850baa38171e4fa45a255d38f0c423aa
zunza <fksfks015@naver.com>
2023-03-03 18:18:30 +0900

    Create PlanetListDto.java

---
+++ src/main/java/com/myproject/planetland/dto/PlanetListDto.java
@@ -0,0 +1,12 @@
+package com.myproject.planetland.dto;
+
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PlanetListDto {
+
+	private String planetName;
+}
