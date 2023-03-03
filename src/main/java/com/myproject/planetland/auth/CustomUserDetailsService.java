6ffe27d0cfa4ee6c07682295f230f357c73d253b
zunza <fksfks015@naver.com>
2023-03-03 18:18:10 +0900

    Update CustomUserDetailsService.java

--- src/main/java/com/myproject/planetland/auth/CustomUserDetailsService.java
+++ src/main/java/com/myproject/planetland/auth/CustomUserDetailsService.java
@@ -5,18 +5,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.myproject.planetland.auth.CustomUserDetails;
 import com.myproject.planetland.domain.User;
 import com.myproject.planetland.repository.UserRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class CustomUserDetailsService implements UserDetailsService {
 	@Autowired UserRepository userRepository;
 
 	@Override
-	public UserDetails loadUserByUsername(String userName) {
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+		log.info("username = {}", userName);
 		Optional<User> res = userRepository.findByUserName(userName);
 		if (res.isPresent()) {
 			return new CustomUserDetails(res.get());
