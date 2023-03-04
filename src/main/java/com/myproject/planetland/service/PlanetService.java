ad041627a63df5f63b2bd0f11b7c94cbdac1a416
zunza <fksfks015@naver.com>
2023-03-04 22:14:37 +0900

    Update PlanetService.java

--- src/main/java/com/myproject/planetland/service/PlanetService.java
+++ src/main/java/com/myproject/planetland/service/PlanetService.java
@@ -1,23 +1,37 @@
 package com.myproject.planetland.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import javax.persistence.EntityNotFoundException;
 
+import org.codehaus.groovy.tools.shell.IO;
+import org.hibernate.search.backend.PurgeAllLuceneWork;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.myproject.planetland.domain.Planet;
 import com.myproject.planetland.dto.AddPlanetDto;
+import com.myproject.planetland.dto.PlanetDto;
+import com.myproject.planetland.mapper.PlanetMapper;
 import com.myproject.planetland.repository.PlanetRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PlanetService {
 
 	private final PlanetRepository planetRepository;
+	private final PlanetMapper mapper;
 
 	public Planet getPlanet(Long planetId) {
 		Optional<Planet> res = planetRepository.findById(planetId);
@@ -52,12 +66,43 @@ public class PlanetService {
 		return planetList;
 	}
 
-	public AddPlanetDto createPlanet(AddPlanetDto addPlanetDto) {
+	public AddPlanetDto createPlanet(AddPlanetDto addPlanetDto, MultipartFile imgFile) throws IOException {
 		Optional<Planet> res = planetRepository.findByPlanetName(addPlanetDto.getPlanetName());
 		if (res.isPresent()) {
 			throw new IllegalArgumentException("이미 존재하는 행성입니다.");
 		} else {
+			String oriName = imgFile.getOriginalFilename();
+			String myPath = System.getProperty("user.dir") + "/src/main/resources/static/images/";
+
+			UUID uuid = UUID.randomUUID(); // 파일이름 중복을 피하기 위한 식별자 역할
+			String savedFileName = uuid + "_" + oriName;
+
+			imgFile.transferTo(new File(myPath+savedFileName));
+
+			addPlanetDto.setImgName(savedFileName);
+			addPlanetDto.setImgPath("/images/"+savedFileName);
+
+			Planet planet = mapper.addPlanetDtoToModel(addPlanetDto);
+			planetRepository.save(planet);
+
 			return addPlanetDto;
+		}
+	}
+
+	public AddPlanetDto updatePlanet(Long planetId, AddPlanetDto addPlanetDto) {
+		Optional<Planet> res = planetRepository.findById(planetId);
+		if (res.isPresent()) {
+			Planet updatePlanet = res.get();
+			updatePlanet.setPlanetName(addPlanetDto.getPlanetName());
+			updatePlanet.setPopulation(addPlanetDto.getPopulation());
+			updatePlanet.setValue(addPlanetDto.getValue());
+			updatePlanet.setImgPath(addPlanetDto.getImgPath());
+			updatePlanet.setPlanetStatus(addPlanetDto.getPlanetStatus());
+			Planet savedPlanet = planetRepository.save(updatePlanet);
+			AddPlanetDto updatePlantDto = mapper.ModelToAddPlanetDto(savedPlanet);
+			return updatePlantDto;
+		} else {
+			throw new IllegalArgumentException("잘못된 경로입니다.");
 		}
 	}
 }
