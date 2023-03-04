39a10856d82a789d2d16c3079dcbd1be938125b9
zunza <fksfks015@naver.com>
2023-03-04 22:14:27 +0900

    Update PlanetListDto.java

--- src/main/java/com/myproject/planetland/dto/PlanetListDto.java
+++ src/main/java/com/myproject/planetland/dto/PlanetListDto.java
@@ -2,11 +2,18 @@ package com.myproject.planetland.dto;
 
 import javax.validation.constraints.NotEmpty;
 
+import com.myproject.planetland.constants.PlanetStatus;
+
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
-@Getter @Setter
+@Getter @Setter @ToString
 public class PlanetListDto {
 
+	private Long planetId;
 	private String planetName;
+	private PlanetStatus planetStatus;
+	private String imgPath;
+	private String imgName;
 }
