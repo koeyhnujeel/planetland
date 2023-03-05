2b5b2f29e20fddbebee73e39caa2a8358c433543
zunza <fksfks015@naver.com>
2023-03-06 00:17:47 +0900

    Update PlanetService.java

--- src/main/java/com/myproject/planetland/service/PlanetService.java
+++ src/main/java/com/myproject/planetland/service/PlanetService.java
@@ -2,23 +2,19 @@ package com.myproject.planetland.service;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.persistence.EntityNotFoundException;
 
-import org.codehaus.groovy.tools.shell.IO;
-import org.hibernate.search.backend.PurgeAllLuceneWork;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.myproject.planetland.constants.ImgDir;
 import com.myproject.planetland.domain.Planet;
 import com.myproject.planetland.dto.AddPlanetDto;
-import com.myproject.planetland.dto.PlanetDto;
+import com.myproject.planetland.dto.PlanetListDto;
 import com.myproject.planetland.mapper.PlanetMapper;
 import com.myproject.planetland.repository.PlanetRepository;
 
@@ -60,10 +56,24 @@ public class PlanetService {
 		}
 	}
 
-	public List<Planet> getAllPlanet() {
-		List<Planet> planetList = planetRepository.findAll();
+	public List<PlanetListDto> getAllPlanet(String keyword) {
+		if(keyword.equals("기본 순")) {
+			List<Planet> planetList = planetRepository.findAll();
+			List<PlanetListDto> planetListDtos = mapper.ModelToDtoList(planetList);
+			return planetListDtos;
 
-		return planetList;
+		} else if (keyword.equals("높은 가치 순")) {
+			List<Planet> allOrderByValueDesc = planetRepository.findAllByOrderByValueDesc();
+			List<PlanetListDto> planetListDtosDesc = mapper.ModelToDtoList(allOrderByValueDesc);
+			return planetListDtosDesc;
+
+		} else if (keyword.equals("낮은 가치 순")) {
+			List<Planet> allOrderByValueAsc = planetRepository.findAllByOrderByValueAsc();
+			List<PlanetListDto> planetListDtosAsc = mapper.ModelToDtoList(allOrderByValueAsc);
+			return planetListDtosAsc;
+		} else {
+			throw new IllegalArgumentException("잘못된 정렬입니다.");
+		}
 	}
 
 	public AddPlanetDto createPlanet(AddPlanetDto addPlanetDto, MultipartFile imgFile) throws IOException {
@@ -72,7 +82,7 @@ public class PlanetService {
 			throw new IllegalArgumentException("이미 존재하는 행성입니다.");
 		} else {
 			String oriName = imgFile.getOriginalFilename();
-			String myPath = System.getProperty("user.dir") + "/src/main/resources/static/images/";
+			String myPath = ImgDir.PATH;
 
 			UUID uuid = UUID.randomUUID(); // 파일이름 중복을 피하기 위한 식별자 역할
 			String savedFileName = uuid + "_" + oriName;
@@ -104,5 +114,19 @@ public class PlanetService {
 		} else {
 			throw new IllegalArgumentException("잘못된 경로입니다.");
 		}
+	}
+	public void deletePlanet(Long planetId) throws IOException {
+		Optional<Planet> res = planetRepository.findById(planetId);
+		if (res.isEmpty()) {
+			throw new EntityNotFoundException(String.format("아이디 %d로 조회되는 행성이 없습니다.", planetId));
+		}
+
+		String myPath = ImgDir.PATH;
+		String fileName = res.get().getImgName();
+		File file = new File(myPath + fileName);
+		if (fileName != null) {
+			file.delete();
+		}
+		planetRepository.deleteById(planetId);
 	}
 }
