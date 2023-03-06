3cc189a710ed74e0cd37125463cdfde8457c74c3
zunza <fksfks015@naver.com>
2023-03-07 01:59:10 +0900

    Update MainController.java

--- src/main/java/com/myproject/planetland/controller/MainController.java
+++ src/main/java/com/myproject/planetland/controller/MainController.java
@@ -1,12 +1,15 @@
 package com.myproject.planetland.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.myproject.planetland.auth.CustomUserDetails;
 import com.myproject.planetland.dto.PlanetListDto;
 import com.myproject.planetland.service.PlanetService;
 
@@ -16,7 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class MainController {
 
-	@Autowired PlanetService planetService;
+	@Autowired
+	PlanetService planetService;
 
 	@GetMapping(value = {"/", "/sort"})
 	public String home(@RequestParam(required = false, defaultValue = "기본 순") String keyword, Model model) {
