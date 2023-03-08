156c00bc4f2b5c45c5c056ee20c3979f097bb673
zunza <fksfks015@naver.com>
2023-03-09 03:26:17 +0900

    Update PlanetService.java

--- src/main/java/com/myproject/planetland/service/PlanetService.java
+++ src/main/java/com/myproject/planetland/service/PlanetService.java
@@ -13,12 +13,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.myproject.planetland.constants.ImgDir;
 import com.myproject.planetland.constants.PlanetStatus;
+import com.myproject.planetland.constants.Type;
+import com.myproject.planetland.domain.OrderHis;
 import com.myproject.planetland.domain.Planet;
 import com.myproject.planetland.domain.User;
 import com.myproject.planetland.dto.AddPlanetDto;
+import com.myproject.planetland.dto.MyPlanetsDto;
+import com.myproject.planetland.dto.PlanetDto;
 import com.myproject.planetland.dto.PlanetListDto;
 import com.myproject.planetland.mapper.PlanetMapper;
 import com.myproject.planetland.repository.PlanetRepository;
+import com.myproject.planetland.repository.OrderHisRepository;
 import com.myproject.planetland.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -31,23 +36,31 @@ public class PlanetService {
 
 	private final PlanetRepository planetRepository;
 	private final UserRepository userRepository;
+	private final OrderHisRepository hisRepository;
 	private final PlanetMapper mapper;
 
-	public Planet getPlanet(Long planetId) {
+	public AddPlanetDto getEditPlanet(Long planetId) {
 		Optional<Planet> res = planetRepository.findById(planetId);
 		if (res.isPresent()) {
-			return res.get();
-		} else {
+			Planet planet = res.get();
+			AddPlanetDto addPlanetDto = mapper.modelToAddPlanetDto(planet);
+			return addPlanetDto;
+		}else {
 			throw new EntityNotFoundException(String.format("아이디 %d로 조회되는 행성이 없습니다.", planetId));
 		}
 	}
 
-	public Planet getPlanet(String planetName) {
-		Optional<Planet> res = planetRepository.findByPlanetName(planetName);
+	public PlanetDto getPlanetDetail(Long planetId) {
+		Optional<Planet> res = planetRepository.findById(planetId);
 		if (res.isPresent()) {
-			return res.get();
+			Planet planet = res.get();
+			PlanetDto planetDto = mapper.modelToDto(planet);
+			if (planet.getUser() != null) {
+				planetDto.setOwner(planet.getUser().getUserName());
+			}
+			return planetDto;
 		} else {
-			throw new EntityNotFoundException(String.format("%s는 존재하지 않는 행성입니다.", planetName));
+			throw new EntityNotFoundException(String.format("아이디 %d로 조회되는 행성이 없습니다.", planetId));
 		}
 	}
 
@@ -60,20 +73,25 @@ public class PlanetService {
 		}
 	}
 
+	public List<MyPlanetsDto> getMyPlanets(Long userId) {
+		List<Planet> res = planetRepository.findByUser_userId(userId);
+		return mapper.modelToMyPlanetsDto(res);
+	}
+
 	public List<PlanetListDto> getAllPlanet(String keyword) {
 		if (keyword.equals("기본 순")) {
 			List<Planet> planetList = planetRepository.findAll();
-			List<PlanetListDto> planetListDtos = mapper.ModelToDtoList(planetList);
+			List<PlanetListDto> planetListDtos = mapper.modelToDtoList(planetList);
 			return planetListDtos;
 
 		} else if (keyword.equals("높은 가치 순")) {
 			List<Planet> allOrderByValueDesc = planetRepository.findAllByOrderByValueDesc();
-			List<PlanetListDto> planetListDtosDesc = mapper.ModelToDtoList(allOrderByValueDesc);
+			List<PlanetListDto> planetListDtosDesc = mapper.modelToDtoList(allOrderByValueDesc);
 			return planetListDtosDesc;
 
 		} else if (keyword.equals("낮은 가치 순")) {
 			List<Planet> allOrderByValueAsc = planetRepository.findAllByOrderByValueAsc();
-			List<PlanetListDto> planetListDtosAsc = mapper.ModelToDtoList(allOrderByValueAsc);
+			List<PlanetListDto> planetListDtosAsc = mapper.modelToDtoList(allOrderByValueAsc);
 			return planetListDtosAsc;
 		} else {
 			throw new IllegalArgumentException("잘못된 정렬입니다.");
@@ -85,15 +103,15 @@ public class PlanetService {
 		if (res.isPresent()) {
 			throw new IllegalArgumentException("이미 존재하는 행성입니다.");
 		} else {
-			String savedFileName = getFileName(imgFile);
-
-			addPlanetDto.setImgName(savedFileName);
-			addPlanetDto.setImgPath("/images/" + savedFileName);
-
+			if(!imgFile.isEmpty()) {
+				String savedFileName = getFileName(imgFile);
+				addPlanetDto.setImgName(savedFileName);
+				addPlanetDto.setImgPath("/images/" + savedFileName);
+			}
 			Planet planet = mapper.addPlanetDtoToModel(addPlanetDto);
 			Planet savedPlanet = planetRepository.save(planet);
 
-			return mapper.ModelToAddPlanetDto(savedPlanet);
+			return mapper.modelToAddPlanetDto(savedPlanet);
 		}
 	}
 
@@ -105,7 +123,7 @@ public class PlanetService {
 			updatePlanet.setPopulation(addPlanetDto.getPopulation());
 			updatePlanet.setValue(addPlanetDto.getValue());
 			updatePlanet.setPlanetStatus(addPlanetDto.getPlanetStatus());
-			if (imgFile != null) {
+			if (!imgFile.isEmpty()) {
 				if (updatePlanet.getImgPath() != null) {
 					String myPath = ImgDir.PATH;
 					File file1 = new File(myPath, updatePlanet.getImgName());
@@ -117,7 +135,7 @@ public class PlanetService {
 			}
 
 			Planet savedPlanet = planetRepository.save(updatePlanet);
-			AddPlanetDto updatePlantDto = mapper.ModelToAddPlanetDto(savedPlanet);
+			AddPlanetDto updatePlantDto = mapper.modelToAddPlanetDto(savedPlanet);
 			return updatePlantDto;
 		} else {
 			throw new IllegalArgumentException("잘못된 경로입니다.");
@@ -146,25 +164,32 @@ public class PlanetService {
 			throw new IllegalArgumentException("잘못된 경로입니다.");
 		}
 
-		User user = resUser.get();
+		User buyer = resUser.get();
 		Planet planet = resPlanet.get();
 
 		if (planet.getUser() != null) {
-			User res = planet.getUser();
-			List<Planet> planets = res.getPlanets();
+			User seller = planet.getUser();
+			List<Planet> planets = seller.getPlanets();
 			for (int i = 0; i < planets.size(); i++) {
 				if (planets.get(i).getPlanetName().equals(planet.getPlanetName())) {
 					planets.remove(i);
-					res.setPlanets(planets);
-					userRepository.save(res);
+					seller.setPlanets(planets);
+					seller.setAsset(seller.getAsset() + planet.getValue());
+					OrderHis his = createOrderHis(seller, planet);
+					his.setType(Type.SELL);
+					hisRepository.save(his);
+					userRepository.save(seller);
 				}
 			}
 		}
-		planet.setUser(user);
+		planet.setUser(buyer);
 		planet.setPlanetStatus(PlanetStatus.SOLD_OUT);
-		user.setAsset(user.getAsset() - planet.getValue());
-		user.getPlanets().add(planet);
-		userRepository.save(user);
+		buyer.setAsset(buyer.getAsset() - planet.getValue());
+		buyer.getPlanets().add(planet);
+		OrderHis his = createOrderHis(buyer, planet);
+		his.setType(Type.BUY);
+		hisRepository.save(his);
+		userRepository.save(buyer);
 		planetRepository.save(planet);
 	}
 
@@ -198,5 +223,14 @@ public class PlanetService {
 
 		imgFile.transferTo(new File(myPath + savedFileName));
 		return savedFileName;
+	}
+
+	private static OrderHis createOrderHis(User user, Planet planet) {
+		OrderHis his = new OrderHis();
+		his.setPlanetName(planet.getPlanetName());
+		his.setValue(planet.getValue());
+		his.setUser(user);
+		user.getOrderHis().add(his);
+		return his;
 	}
 }
