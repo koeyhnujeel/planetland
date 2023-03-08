a0fe810327e4ae27cf080f704f8cf1784bd4e200
zunza <fksfks015@naver.com>
2023-03-09 03:26:34 +0900

    Update User.java

--- src/main/java/com/myproject/planetland/domain/User.java
+++ src/main/java/com/myproject/planetland/domain/User.java
@@ -3,7 +3,6 @@ package com.myproject.planetland.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -12,9 +11,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.myproject.planetland.constants.Role;
@@ -45,5 +42,8 @@ public class User {
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
 	private List<Planet> planets = new ArrayList<>();
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
+	private List<OrderHis> orderHis = new ArrayList<>();
 
 }
