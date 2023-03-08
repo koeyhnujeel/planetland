cc622c13446a8c9984f6319a0c69437c1c0ab26b
zunza <fksfks015@naver.com>
2023-03-09 03:26:47 +0900

    Create TransactionHisMapperImpl.java

---
+++ src/main/generated/com/myproject/planetland/mapper/TransactionHisMapperImpl.java
@@ -0,0 +1,13 @@
+package com.myproject.planetland.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-03-09T02:23:44+0900",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
+)
+@Component
+public class TransactionHisMapperImpl implements TransactionHisMapper {
+}
