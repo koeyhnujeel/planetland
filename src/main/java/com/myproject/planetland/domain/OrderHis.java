49fea3e9434a2b9abb0d82beea1095e13c35c0fa
zunza <fksfks015@naver.com>
2023-03-13 23:20:08 +0900

    Update OrderHis.java

--- src/main/java/com/myproject/planetland/domain/OrderHis.java
+++ src/main/java/com/myproject/planetland/domain/OrderHis.java
@@ -1,9 +1,11 @@
 package com.myproject.planetland.domain;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
@@ -15,6 +17,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.myproject.planetland.constants.Type;
 
@@ -26,6 +31,7 @@ import lombok.ToString;
 @Entity
 @Getter @Setter @ToString
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "transaction_his", schema = "planet_land")
 public class OrderHis {
 
@@ -37,11 +43,12 @@ public class OrderHis {
 	@Enumerated(EnumType.STRING)
 	private Type type;
 
-	@CreationTimestamp
-	private Date date;
+	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@CreatedDate
+	private LocalDateTime date;
 
 	private String planetName;
-	private int value;
+	private int price;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_id")
