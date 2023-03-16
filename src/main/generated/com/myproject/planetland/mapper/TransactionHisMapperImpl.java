d7813ae4d5e8c779a44063063f00ea0b3f5c74f5
zunza <fksfks015@naver.com>
2023-03-17 06:51:37 +0900

    Update TransactionHisMapperImpl.java

--- src/main/generated/com/myproject/planetland/mapper/TransactionHisMapperImpl.java
+++ src/main/generated/com/myproject/planetland/mapper/TransactionHisMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-03-15T00:50:25+0900",
+    date = "2023-03-16T23:43:16+0900",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.17 (Azul Systems, Inc.)"
 )
 @Component
